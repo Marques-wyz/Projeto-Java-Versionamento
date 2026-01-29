38076711978b9e2b79a607c57aade775affc42ac
Caua Marques <cauamarques09sp@gmail.com>
2026-01-29 20:52:00 -0300

    Adiciona mensagens de suscesso ou erro no cadastro de itens

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -23,7 +23,7 @@ public class conectaDAO {
         try {
         
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
+            //Exibe mensagem informando sucesso ou erro no cadastro 
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
